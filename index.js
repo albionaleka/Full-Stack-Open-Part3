f263c071bc6216b80b6b03a50904f6f5645949e9
Albiona <albionnalekaa@gmail.com>
2024-12-06 16:31:03 +0100

    3.15-3.18

--- index.js
+++ index.js
@@ -16,29 +16,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('dist'));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 app.get('/', (request, response) => {
     response.send('<h1>Express Server</h1>');
 });
@@ -49,10 +26,11 @@ app.get('/api/persons', (request, response) => {
     });
 });
 
-app.get('/info', (request, response) => {
+app.get('/info', async (request, response) => {
     let time = new Date().toString();
+    const count = await Person.countDocuments();
 
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    response.send(`<p>Phonebook has info for ${count} people</p>
         <p>${time}</p>`);
 });
 
@@ -61,17 +39,16 @@ app.get('/api/persons/:id', (request, response) => {
     
     Person.findById(id).then(person => {
         response.json(person);
-    })
+    });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end();
+    }).catch (error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', async (request, response) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -80,26 +57,40 @@ app.post('/api/persons', (request, response) => {
         });
     }
 
-    // const nameExists = persons.some(person => person.name === body.name);
-    // if (nameExists) {
-    //     return response.status(400).json({ 
-    //         error: 'Name must be unique' 
-    //     });
-    // }
+    const nameExists = await Person.findOne({name: body.name});
+    
+    if (nameExists) {
+        const newInfo = {
+            name: body.name,
+            number: body.number
+        }
 
-    const person = new Person ({
-        name: body.name,
-        number: body.number,
-    });
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson);
-    });
+        Person.findByIdAndUpdate(nameExists._id, newInfo, { new: true }).then(updated => {
+            response.json(updated);
+        }).catch (error => next);
+    } else {
+        const person = new Person ({
+            name: body.name,
+            number: body.number,
+        });
+    
+        person.save().then(savedPerson => {
+            response.json(savedPerson);
+        });
+    }
 });
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1000000);
-};
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
