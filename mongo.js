4b397c6cf196254872068813ca2b0cb2671967d2
Albiona <albionnalekaa@gmail.com>
2024-12-06 11:47:43 +0100

    3.12

---
+++ mongo.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2]
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const url = `mongodb+srv://albiona:${password}@cluster0.m8yma.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+const connect = async () => {
+    try {
+        await mongoose.connect(url)
+        console.log("Connected to MongoDB")
+    } catch (err) {
+        console.log("There was an error connecting to the database.", err)
+    }
+}
+
+if (process.argv.length === 5) {
+    let name = process.argv[3]
+    let number = process.argv[4]
+
+    connect()
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(() => {
+        console.log(`Added ${name}, number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+} else if (process.argv.length === 3) {
+    connect()
+
+    Person.find({}).then(result => {
+        if (result.length === 0) {
+            console.log("Phonebook is empty")
+        } else {
+            console.log("Phonebook:")
+
+            result.forEach(entry => {
+                console.log(`${entry.name} ${entry.number}`)
+            });
+        }
+        mongoose.connection.close()
+    })
+
+} else if (process.argv.length < 3) {
+    console.log("Please provide password as an argument.")
+    process.exit(1)
+}
